93e1d2f6cad7e4ac186af853bbb07dc39f075a21
misha <shma2001@gmail.com>
2016-03-06 02:06:28 +0600

    ok2

---
+++ src/test/java/org/misha/logical/tree/impl/SearcherTest.java
@@ -0,0 +1,12 @@
+package org.misha.logical.tree.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearcherTest {
+
+    @Test
+    public void testSearch() throws Exception {
+    }
+}
